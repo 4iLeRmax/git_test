08e169c93847be4552b38c718081b9fa5179a38e
4iLeRmax <max.zlatin@gmail.com>
2023-07-31 19:08:17 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <>
       <div>Work !!!</div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, qui!</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, qui!
+      </p>
     </>
   );
 };
