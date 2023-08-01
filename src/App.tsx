e26a9ac669064953eee22591870d7d875a2d6a87
4iLeRmax <max.zlatin@gmail.com>
2023-08-01 18:05:26 +0300

    alt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import React from 'react';
+
+const App = () => {
   return (
     <>
-      sdhfisgdfgsdufg !!!!!!!!!
+      <div>Work !!!</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, qui!
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
